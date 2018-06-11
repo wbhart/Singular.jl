2a83872fa40e866925120cff9242664e774243db
William Hart <goodwillhart@googlemail.com>
2018-06-11 17:22:27 +0200

    Add matrix documentation.

--- search_index.js
+++ search_index.js
@@ -1096,4 +1096,28 @@ var documenterSearchIndex = {"docs": [
     "text": "minres{T <: AbstractAlgebra.RingElem}(::sresolution{T})ExamplesR, (x, y) = PolynomialRing(QQ, [\"x\", \"y\"])\n\nI = Ideal(R, x*y + 1, x^2 + 1)\nF = fres(std(I), 3)\nM = minres(F)"
 },
 
+{
+    "location": "matrix.html#",
+    "page": "Matrices",
+    "title": "Matrices",
+    "category": "page",
+    "text": "CurrentModule = Singular"
+},
+
+{
+    "location": "matrix.html#Matrices-1",
+    "page": "Matrices",
+    "title": "Matrices",
+    "category": "section",
+    "text": "Singular internally allows for matrices over polynomial rings to be created extremely efficiently from ideals and modules (often without copying data). This allows for introspection of modules and operations that can be expressed in terms of matrices (e.g. composition of R-module homomorpisms) to be computed, at a low level.The default matrix type in Singular.jl is the smatrix type.Matrix objects have a parent object which represents the space of matrices they belong to, the data for which is given by the polynomial ring R over which the matrices are defined, and the number of rows and columns of the matrices in the space.The types of matrices and associated parent objects are given in the following table according to the library provding them.Library Element type Parent type\nSingular smatrix{T} Singular.MatrixSpace{T}These types are parameterised by the type of elements in the polynomial ring R over which the matrices are defined.All matrix types belong directly to the abstract type SetElem and all the matrix space parent object types belong to the abstract type Set."
+},
+
+{
+    "location": "matrix.html#Matrix-functionality-1",
+    "page": "Matrices",
+    "title": "Matrix functionality",
+    "category": "section",
+    "text": "Singular.jl matrices implement standard operations one would expect. These include:Operations common to all AbstractAlgebra objects, such as parent, base_ring, elem_type, parent_type, parent, deepcopy, etc.The following parts of the Matrix interface from AbstractAlgebra are also implemented:matrix dimensions: nrows, ncols\naccessing elements of a matrix M[i, j]\niszero\narithmetic operations: +, -, *\ncomparison: ==No other functionality is provided at this time."
+},
+
 ]}
